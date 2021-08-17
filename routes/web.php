755faa6fda51f995f88f5d2e99ce3cfd4943078c
na7esan <na7esan@gmail.com>
2021-08-18 01:32:26 +0900

    init

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+require __DIR__.'/auth.php';
+
+// 認証ルート
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+    Route::get('/task', function () {
+        return 'task route';
+    });
+
+
 });
